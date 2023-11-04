4f6d65e969d99d408ec693f2775acf86cfbe11cc
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-04 17:25:13 +0200

    Create ALLForOne.cs
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

---
+++ Musify.MVC/Repositories/ALLForOne.cs
@@ -0,0 +1,12 @@
+﻿using Musify.MVC.Persistance;
+
+namespace Musify.MVC.Repositories;
+
+public class ALLForOne : IALLForOne
+{
+    public IAlbumRepository AlbumRepository { get; set; }
+    public IArtistRepository ArtistRepository { get; set; }
+    public IAuthorRepository AuthorRepository { get; set; }
+    public IPlaylistRepository PlaylistRepository { get; set; }
+    public ISongRepository SongRepository { get; set; }
+}

5f62f792a5df11aac3f3d237868e45f17ac9d44a
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-04 17:25:08 +0200

    Update Program.cs
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

--- Musify.MVC/Program.cs
+++ Musify.MVC/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Musify.MVC.Data;
+using Musify.MVC.Persistance;
+using Musify.MVC.Profiles;
 using Musify.MVC.Repositories;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +14,12 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<MusifyDbContext>(opt => opt.UseSqlServer((builder.Configuration["ConnectionStrings:DefaultString"])));
 
 
-builder.Services.AddScoped<ISongRepository,SongRepository>(); 
-builder.Services.AddScoped<IPlaylistRepository,PlaylistRepository>();
-builder.Services.AddScoped<IAlbumRepository,AlbumReposioty>();
-
+builder.Services.AddScoped(typeof(IGenaricRepository<>), typeof(Genaricepository<>));
+builder.Services.AddScoped<ISongRepository, SongRepository>();
+builder.Services.AddScoped<IPlaylistRepository, PlaylistRepository>();
+builder.Services.AddScoped<IAlbumRepository, AlbumReposioty>();
+builder.Services.AddScoped<IALLForOne, ALLForOne>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 
 var app = builder.Build();

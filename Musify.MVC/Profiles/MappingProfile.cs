cd86af0bac526149be99df9396946d9014869f93
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-04 17:25:05 +0200

    Create MappingProfile.cs
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

---
+++ Musify.MVC/Profiles/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Musify.MVC.Models.Entities;
+using Musify.MVC.Models.ModelViews.SongModelView;
+
+namespace Musify.MVC.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Song, SongViewModel>().ReverseMap();
+    }
+}

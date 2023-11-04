4f28ee1cd0617163dc9babc0e425efe03c244ad7
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-04 17:24:57 +0200

    Update ISongRepository.cs
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

--- Musify.MVC/Persistance/ISongRepository.cs
+++ Musify.MVC/Persistance/ISongRepository.cs
@@ -5,6 +5,7 @@ namespace Musify.MVC.Repositories
     public interface ISongRepository : IGenaricRepository<Song>
     {
         public Task<Song> GetSongWithArtist(Guid id);
+        public Task<List<Song>> GetLatestReleased6Songs();
 
     }
 }

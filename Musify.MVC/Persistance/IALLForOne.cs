3fa39a6fcb22d5b79ed4e97aff7cf29925a92e20
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-04 17:24:54 +0200

    Create IALLForOne.cs
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

---
+++ Musify.MVC/Persistance/IALLForOne.cs
@@ -0,0 +1,13 @@
+﻿using Musify.MVC.Repositories;
+
+namespace Musify.MVC.Persistance;
+
+public interface IALLForOne
+{
+    public IAlbumRepository AlbumRepository { get; set; }
+    public IArtistRepository ArtistRepository { get; set; }
+    public IAuthorRepository AuthorRepository { get; set; }
+    public IPlaylistRepository PlaylistRepository { get; set; }
+    public ISongRepository SongRepository { get; set; }
+
+}

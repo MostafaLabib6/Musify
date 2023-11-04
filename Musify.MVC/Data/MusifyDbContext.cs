e296841968285f1b49c07ae36e246a823f0d2a3e
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-04 17:24:18 +0200

    Update MusifyDbContext.cs
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

--- Musify.MVC/Data/MusifyDbContext.cs
+++ Musify.MVC/Data/MusifyDbContext.cs
@@ -1,21 +1,26 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Musify.MVC.Models.Entities;
 
 namespace Musify.MVC.Data;
 
-public class MusifyDbContext : DbContext
+public class MusifyDbContext : IdentityDbContext
 {
     public DbSet<Song> Songs { get; set; }
     public DbSet<Playlist> Playlists { get; set; }
     public DbSet<Album> Albums { get; set; }
-
+    public DbSet<Artist> Artists { get; set; }
+    public DbSet<Author> Authors { get; set; }
     public MusifyDbContext(DbContextOptions<MusifyDbContext> options) : base(options)
     {
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Song>()
+        .HasIndex(e => e.ReleaseAt);
     }
 
 }

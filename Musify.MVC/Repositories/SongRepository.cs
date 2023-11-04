76577668f09e5daa84f085b3723736e883b05806
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-04 17:25:15 +0200

    Update SongRepository.cs
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

--- Musify.MVC/Repositories/SongRepository.cs
+++ Musify.MVC/Repositories/SongRepository.cs
@@ -12,9 +12,16 @@ namespace Musify.MVC.Repositories
         {
             _dbContext = dbContext;
         }
+
+        public async Task<List<Song>> GetLatestReleased6Songs()
+        {
+            return await _dbContext.Songs.OrderByDescending(s => s.ReleaseAt).Take(6).Include(x => x.Artists).ToListAsync();
+        }
+
         public Task<Song> GetSongWithArtist(Guid id)
         {
             throw new NotImplementedException();
         }
+
     }
 }

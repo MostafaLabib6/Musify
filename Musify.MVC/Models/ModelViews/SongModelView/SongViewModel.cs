0174deaac3ac39c986bfb89db28ba2b30fbc3371
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-04 17:24:46 +0200

    Create SongViewModel.cs
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

---
+++ Musify.MVC/Models/ModelViews/SongModelView/SongViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Principal;
+
+namespace Musify.MVC.Models.ModelViews.SongModelView
+{
+    public class SongViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Audio { get; set; }
+        public Guid ArtistId { get; set; }
+        public string ArtistName { get; set; }
+    }
+}

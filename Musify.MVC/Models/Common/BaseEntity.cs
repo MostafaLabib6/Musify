ae124ac0c7cc04a1f059078c33135b3421894028
MostafaLabib6 <mostafalabib510@gmail.com>
2023-12-03 18:02:19 +0200

    add validation

--- Musify.MVC/Models/Common/BaseEntity.cs
+++ Musify.MVC/Models/Common/BaseEntity.cs
@@ -1,8 +1,11 @@
-﻿namespace Musify.MVC.Models.Common
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Musify.MVC.Models.Common
 {
     public abstract class BaseEntity
     {
         public Guid Id { get; set; }
+        [Required(ErrorMessage = "Name is Required")] //data annotation
         public string Name { get; set; }
     }
 }

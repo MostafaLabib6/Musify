7e418bc395e9267ec636a4b1dd010a84532912db
MostafaLabib6 <mostafalabib510@gmail.com>
2023-12-03 19:48:57 +0200

    impleament unit of work
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

--- Musify.MVC/Repositories/ALLForOne.cs
+++ Musify.MVC/Repositories/ALLForOne.cs
@@ -1,12 +1,37 @@
-﻿using Musify.MVC.Persistance;
+﻿using Microsoft.EntityFrameworkCore;
+using Musify.MVC.Data;
+using Musify.MVC.Persistance;
 
 namespace Musify.MVC.Repositories;
 
 public class ALLForOne : IALLForOne
 {
-    public IAlbumRepository AlbumRepository { get; set; }
-    public IArtistRepository ArtistRepository { get; set; }
-    public IAuthorRepository AuthorRepository { get; set; }
-    public IPlaylistRepository PlaylistRepository { get; set; }
-    public ISongRepository SongRepository { get; set; }
+    private IAlbumRepository _albumReposioty { get; set; }
+    private IArtistRepository _artistRepository { get; set; }
+    private IAuthorRepository _authorRepository { get; set; }
+    private ISongRepository _songRepository { get; set; }
+    private IPlaylistRepository _playlistRepository { get; set; }
+
+    private readonly MusifyDbContext _dbContext;
+
+    public ALLForOne(MusifyDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public IAlbumRepository AlbumRepository
+    {
+        get
+        { return _albumReposioty ?? new AlbumReposioty(_dbContext); }
+    }
+    public IArtistRepository ArtistRepository { get { return _artistRepository ?? new ArtistRepository(_dbContext); } }
+    public IAuthorRepository AuthorRepository { get { return _authorRepository ?? new AuthorRepository(_dbContext); } }
+    public IPlaylistRepository PlaylistRepository { get { return _playlistRepository ?? new PlaylistRepository(_dbContext); } }
+    public ISongRepository SongRepository { get { return _songRepository ?? new SongRepository(_dbContext); } }
+
+    public void Dispose()
+    {
+        _dbContext?.Dispose();
+        GC.SuppressFinalize(this);
+    }
 }

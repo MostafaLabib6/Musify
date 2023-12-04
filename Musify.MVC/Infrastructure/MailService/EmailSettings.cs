6bc8f2088e0ebd6dd44b731850d417e521c9a3ff
MostafaLabib6 <mostafalabib510@gmail.com>
2023-12-04 20:14:40 +0200

    create Email Settings
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

---
+++ Musify.MVC/Infrastructure/MailService/EmailSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Musify.MVC.Infrastructure.MailService;
+
+public class EmailSettings
+{
+    public string FromAddress { get; set; }
+    public string APIKey { get; set; }
+    public string FromName { get; set; }
+}

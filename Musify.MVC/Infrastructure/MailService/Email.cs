a4553bbe6e0fe9ee83d3e77eeae2a8668eb2b377
MostafaLabib6 <mostafalabib510@gmail.com>
2023-12-04 20:15:38 +0200

    add actual email to be send.
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

---
+++ Musify.MVC/Infrastructure/MailService/Email.cs
@@ -0,0 +1,8 @@
+﻿namespace Musify.MVC.Infrastructure.MailService;
+
+public class Email
+{
+    public string To { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+}

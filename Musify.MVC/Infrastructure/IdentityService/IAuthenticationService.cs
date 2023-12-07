118e451328eb2138f2c13672e5fb850ce3f0065d
MostafaLabib6 <mostafalabib510@gmail.com>
2023-12-07 20:05:02 +0200

    add Authentication Service
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

---
+++ Musify.MVC/Infrastructure/IdentityService/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using Musify.MVC.Models.DTOs;
+
+namespace Musify.MVC.Infrastructure.IdentityService
+{
+    public interface IAuthenticationService
+    {
+        public Task<Result> Login(LoginModel loginModel);
+        public Task Logout();
+        public Task<Result> Register(RegistrationModel registrationModel);
+        public Task<Result> ChangePassword(ChangePassword changePassword);
+    }
+}

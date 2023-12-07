2599a314edd91bc2562948038370ac79dc07ab7e
MostafaLabib6 <mostafalabib510@gmail.com>
2023-12-07 20:05:52 +0200

    create Change Password DTO
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

---
+++ Musify.MVC/Models/DTOs/ChangePassword.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Musify.MVC.Models.DTOs;
+
+public class ChangePassword
+{
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+
+    [Required]
+    public string NewPassword { get; set; }
+
+}

615effb0c0500029dc3332c3ff7ece7e194391f9
MostafaLabib6 <mostafalabib510@gmail.com>
2023-12-07 20:06:13 +0200

    Create Required DTOs
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

---
+++ Musify.MVC/Models/DTOs/RegistrationModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Musify.MVC.Models.DTOs;
+
+public class RegistrationModel
+{
+    [Required]
+    public string UserName { get; set; }
+
+    [Required, EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+
+    [Required, Compare("Password")]
+    public string ConfirmPassword { get; set; }
+
+    public RoleEnum? Role { get; set; }
+}
---
+++ Musify.MVC/Models/DTOs/Result.cs
@@ -0,0 +1,20 @@
+﻿namespace Musify.MVC.Models.DTOs;
+
+public class Result
+{
+    public string Message { get; set; }
+    public bool Success { get; set; }
+}
+
+
+
+/*
+ creation of email 
+   if  email is exist;
+    return result{
+        status = -1;
+        message = "email already taken";
+    }
+ 
+ 
+ */

c64e04a459634b381d9956ede68597f37ab5fdfc
MostafaLabib6 <mostafalabib510@gmail.com>
2023-12-11 22:08:43 +0200

    update authentication service
    Co-Authored-By: Mostafa Ashraf Borhamy <71846308+mostafa500030@users.noreply.github.com>

--- Musify.MVC/Infrastructure/IdentityService/IAuthenticationService.cs
+++ Musify.MVC/Infrastructure/IdentityService/IAuthenticationService.cs
@@ -1,4 +1,5 @@
 ﻿using Musify.MVC.Models.DTOs;
+using Musify.MVC.Models.Entities;
 
 namespace Musify.MVC.Infrastructure.IdentityService
 {
@@ -8,5 +9,12 @@ namespace Musify.MVC.Infrastructure.IdentityService
         public Task Logout();
         public Task<Result> Register(RegistrationModel registrationModel);
         public Task<Result> ChangePassword(ChangePassword changePassword);
+        public Task<Result> ForgetPassword(string email);
+        public Task<Result> Profile(string userId);
+        public Task<string> GenerateToken(User user);
+        public Task<User> FindById(string userId);
+        Task<Result> ConfirmEmail(User user, string token);
+        public Task<bool> DeleteUser(string userId);
     }
 }
+//TODO:external login with facebook and google
